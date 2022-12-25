76b2e95cd5069eccce4fc765a31698e3af1473b1
Lehdari <miika.lehtimaki@gmail.com>
2022-12-25 13:52:07 +0200

    Heatmap: Implement gaussian sample adding

---
+++ include/Heatmap.hpp
@@ -0,0 +1,70 @@
+//
+// Project: DooT2
+// File: Heatmap.hpp
+//
+// Copyright (c) 2022 Miika 'Lehdari' Lehtimäki
+// You may use, distribute and modify this code under the terms
+// of the licence specified in file LICENSE which is distributed
+// with this source code package.
+//
+
+#pragma once
+
+#include "MathTypes.hpp"
+
+#include <opencv2/core/mat.hpp>
+
+
+class Heatmap {
+public:
+    struct Settings {
+        int     resolution; // width and height of the exploration heatmap
+        float   cellSize;   // exploration heatmap cell size
+    };
+
+    Heatmap(const Settings& settings);
+
+    void addSample(const Vec2f& playerPos, float s=1.0f);
+    void addGaussianSample(const Vec2f& playerPos, float s, float sigma);
+    void refreshNormalization();
+
+    inline float sample(const Vec2f& p) const noexcept;
+    inline float normalizedSample(const Vec2f& p) const noexcept;
+
+private:
+    Settings    _settings;
+
+    cv::Mat     _heatmap;
+    float       _heatmapMaxValue;
+    cv::Mat     _heatmapNormalized;
+
+    inline void addSubSample(int x, int y, float s);
+};
+
+
+float Heatmap::sample(const Vec2f& p) const noexcept
+{
+    int px = p(0);
+    int py = p(1);
+    Vec2f frac(p(0)-px, p(1)-py);
+    return (1.0f-frac(0))*(1.0f-frac(1))*_heatmap.at<float>(py, px) +
+           frac(0)*(1.0f-frac(1))*_heatmap.at<float>(py, px+1) +
+           (1.0f-frac(0))*frac(1)*_heatmap.at<float>(py+1, px) +
+           frac(0)*frac(1)*_heatmap.at<float>(py+1, px+1);
+}
+
+float Heatmap::normalizedSample(const Vec2f& p) const noexcept
+{
+    int px = p(0);
+    int py = p(1);
+    Vec2f frac(p(0)-px, p(1)-py);
+    return (1.0f-frac(0))*(1.0f-frac(1))*_heatmapNormalized.at<float>(py, px) +
+           frac(0)*(1.0f-frac(1))*_heatmapNormalized.at<float>(py, px+1) +
+           (1.0f-frac(0))*frac(1)*_heatmapNormalized.at<float>(py+1, px) +
+           frac(0)*frac(1)*_heatmapNormalized.at<float>(py+1, px+1);
+}
+
+void Heatmap::addSubSample(int x, int y, float s)
+{
+    _heatmap.at<float>(y, x) += s;
+}
---
+++ src/Heatmap.cpp
@@ -0,0 +1,66 @@
+//
+// Project: DooT2
+// File: Heatmap.cpp
+//
+// Copyright (c) 2022 Miika 'Lehdari' Lehtimäki
+// You may use, distribute and modify this code under the terms
+// of the licence specified in file LICENSE which is distributed
+// with this source code package.
+//
+
+#include "Heatmap.hpp"
+
+#include <opencv2/highgui.hpp>
+
+
+Heatmap::Heatmap(const Heatmap::Settings &settings) :
+    _settings           (settings),
+    _heatmap            (settings.resolution, settings.resolution, CV_32FC1, 0.0f),
+    _heatmapMaxValue    (0.0f),
+    _heatmapNormalized  (settings.resolution, settings.resolution, CV_32FC1, 0.0f)
+{
+}
+
+void Heatmap::addSample(const Vec2f& playerPos, float s)
+{
+    int heatmapMiddle = _settings.resolution / 2;
+    Vec2f playerPosMap = playerPos / _settings.cellSize + Vec2f(heatmapMiddle, heatmapMiddle);
+
+    int px = playerPosMap(0);
+    int py = playerPosMap(1);
+    Vec2f frac(playerPosMap(0)-px, playerPosMap(1)-py);
+    addSubSample(px, py, s*(1.0f-frac(0))*(1.0f-frac(1)));
+    addSubSample(px+1, py, s*frac(0)*(1.0f-frac(1)));
+    addSubSample(px, py+1, s*(1.0f-frac(0))*frac(1));
+    addSubSample(px+1, py+1, s*frac(0)*frac(1));
+
+    float h = sample(playerPosMap);
+    if (h > _heatmapMaxValue) {
+        _heatmapMaxValue = h;
+    }
+}
+
+void Heatmap::addGaussianSample(const Vec2f& playerPos, float s, float sigma)
+{
+    float nSigma = sigma / _settings.cellSize;
+    int r = std::ceil(nSigma*3.0f);
+
+    // generate gaussian distribution
+    float scale = (1.0f / (nSigma*std::sqrt(2.0f*(float)M_PI)));
+    static std::vector<float> gaussian(2*r+1, 0.0f);
+    gaussian.resize(2*r+1);
+    for (int i=0; i<gaussian.size(); ++i) {
+        gaussian[i] = scale*std::exp(-0.5f*std::pow((i-r)/nSigma, 2.0f));
+    }
+
+    for (int j=-r; j<=r; ++j) {
+        for (int i=-r; i<=r; ++i) {
+            addSample(playerPos+_settings.cellSize*Vec2f(i,j), s*gaussian[i+r]*gaussian[j+r]);
+        }
+    }
+}
+
+void Heatmap::refreshNormalization()
+{
+    _heatmapNormalized = _heatmap / _heatmapMaxValue;
+}
