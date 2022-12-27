3c6df14e9aa2de0446a3eac11c0be4c920ccf8cb
Lehdari <miika.lehtimaki@gmail.com>
2022-12-27 12:10:31 +0200

    Implement a less sucky random walker
    This version reuses actions when moving to regions that have been visited less often.
    It also includes a rudimentary wall-hit detection and door-traversal routine.

--- include/ActionManager.hpp
+++ include/ActionManager.hpp
@@ -12,6 +12,7 @@
 
 
 #include "ActionConverter.hpp"
+#include "MathTypes.hpp"
 
 #include <random>
 
@@ -24,18 +25,29 @@ public:
     ActionManager();
 
     // Add heatmap module
-    void setHeatmap(const Heatmap* heatmap);
+    void setHeatmap(Heatmap* heatmap);
 
-    gvizdoom::Action operator()();
+    void reset();
+
+    gvizdoom::Action operator()(const Vec2f& playerPos);
+
+    float                                   _heatmapDiff;
 
 private:
     ActionConverter<float>                  _actionConverter;
+    std::vector<float>                      _actionVector;
 
     // modules
-    const Heatmap*                          _heatmap;
+    Heatmap*                                _heatmap;
+    float                                   _heatmapSamplePrev;
+    Vec2f                                   _pPrev;
+    float                                   _sPrev;
+
+    int                                     _forwardHoldTimer;
+    int                                     _wallHitTimer;
 
     static std::default_random_engine       _rnd;
     static std::normal_distribution<float>  _rndNormal;
 
-    gvizdoom::Action generateRandomAction();
+    void updateActionVector(float smoothing, float sigma);
 };
