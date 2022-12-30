cc2e971d898692db3715e412fab42ece79f71c0b
Lehdari <miika.lehtimaki@gmail.com>
2022-12-30 19:19:09 +0200

    AutoEncoder: Add layers to encode down to 4x4x128 latent space

--- src/AutoEncoder.cpp
+++ src/AutoEncoder.cpp
@@ -27,7 +27,15 @@ AutoEncoderImpl::AutoEncoderImpl() :
     _bnEnc5         (nn::BatchNorm2dOptions(256)),
     _conv6          (nn::Conv2dOptions(256, 512, {4, 4}).stride({2, 2}).bias(false).padding({1, 1})),
     _bnEnc6         (nn::BatchNorm2dOptions(512)),
+    _conv7          (nn::Conv2dOptions(512, 512, {2, 2}).bias(false)),
+    _bnEnc7         (nn::BatchNorm2dOptions(512)),
+    _conv8          (nn::Conv2dOptions(512, 128, {1, 1}).bias(false)),
+    _bnEnc8         (nn::BatchNorm2dOptions(128)),
 
+    _convTranspose8 (nn::ConvTranspose2dOptions(128, 512, {1, 1})),
+    _bnDec8         (nn::BatchNorm2dOptions(512)),
+    _convTranspose7 (nn::ConvTranspose2dOptions(512, 512, {2, 2})),
+    _bnDec7         (nn::BatchNorm2dOptions(512)),
     _convTranspose6 (nn::ConvTranspose2dOptions(512, 256, {4, 4}).stride({2, 2})),
     _bnDec6         (nn::BatchNorm2dOptions(256)),
     _convTranspose5 (nn::ConvTranspose2dOptions(256, 128, {4, 4}).stride({2, 2})),
@@ -51,6 +59,14 @@ AutoEncoderImpl::AutoEncoderImpl() :
     register_module("bnEnc5", _bnEnc5);
     register_module("conv6", _conv6);
     register_module("bnEnc6", _bnEnc6);
+    register_module("conv7", _conv7);
+    register_module("bnEnc7", _bnEnc7);
+    register_module("conv8", _conv8);
+    register_module("bnEnc8", _bnEnc8);
+    register_module("convTranspose8", _convTranspose8);
+    register_module("bnDec8", _bnDec8);
+    register_module("convTranspose7", _convTranspose7);
+    register_module("bnDec7", _bnDec7);
     register_module("convTranspose6", _convTranspose6);
     register_module("bnDec6", _bnDec6);
     register_module("convTranspose5", _convTranspose5);
@@ -68,14 +84,18 @@ torch::Tensor AutoEncoderImpl::forward(torch::Tensor x)
     using namespace torch::indexing;
 
     // Encoder
-    x = torch::tanh(_bnEnc1(_conv1(x))); // 320x240
-    x = torch::tanh(_bnEnc2(_conv2(x))); // 80x80
-    x = torch::tanh(_bnEnc3(_conv3(x))); // 40x40
-    x = torch::tanh(_bnEnc4(_conv4(x))); // 20x20
-    x = torch::tanh(_bnEnc5(_conv5(x))); // 10x10
-    x = torch::tanh(_bnEnc6(_conv6(x))); // 5x5
+    x = torch::tanh(_bnEnc1(_conv1(x))); // 320x240x8
+    x = torch::tanh(_bnEnc2(_conv2(x))); // 80x80x32
+    x = torch::tanh(_bnEnc3(_conv3(x))); // 40x40x64
+    x = torch::tanh(_bnEnc4(_conv4(x))); // 20x20x128
+    x = torch::tanh(_bnEnc5(_conv5(x))); // 10x10x256
+    x = torch::tanh(_bnEnc6(_conv6(x))); // 5x5x512
+    x = torch::tanh(_bnEnc7(_conv7(x))); // 4x4x512
+    x = torch::tanh(_bnEnc8(_conv8(x))); // 4x4x128
 
     // Decoder
+    x = torch::tanh(_bnDec8(_convTranspose8(x)));
+    x = torch::tanh(_bnDec7(_convTranspose7(x)));
     x = torch::tanh(_bnDec6(_convTranspose6(x)));
     x = x.index({Slice(), Slice(), Slice(1, -1, None), Slice(1, -1, None)});
     x = torch::tanh(_bnDec5(_convTranspose5(x)));
