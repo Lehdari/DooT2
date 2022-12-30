36eddd9dde07fe0c6b461893fba6b1fe6135932e
Lehdari <miika.lehtimaki@gmail.com>
2022-12-30 04:48:14 +0200

    AutoEncoder: Add layers

--- include/AutoEncoder.hpp
+++ include/AutoEncoder.hpp
@@ -20,7 +20,28 @@ public:
     torch::Tensor forward(torch::Tensor x);
 
 private:
-    torch::nn::Conv2d           _conv;
-    torch::nn::ConvTranspose2d  _convTranspose;
+    torch::nn::Conv2d           _conv1;
+    torch::nn::BatchNorm2d      _bnEnc1;
+    torch::nn::Conv2d           _conv2;
+    torch::nn::BatchNorm2d      _bnEnc2;
+    torch::nn::Conv2d           _conv3;
+    torch::nn::BatchNorm2d      _bnEnc3;
+    torch::nn::Conv2d           _conv4;
+    torch::nn::BatchNorm2d      _bnEnc4;
+    torch::nn::Conv2d           _conv5;
+    torch::nn::BatchNorm2d      _bnEnc5;
+    torch::nn::Conv2d           _conv6;
+    torch::nn::BatchNorm2d      _bnEnc6;
+
+    torch::nn::ConvTranspose2d  _convTranspose6;
+    torch::nn::BatchNorm2d      _bnDec6;
+    torch::nn::ConvTranspose2d  _convTranspose5;
+    torch::nn::BatchNorm2d      _bnDec5;
+    torch::nn::ConvTranspose2d  _convTranspose4;
+    torch::nn::BatchNorm2d      _bnDec4;
+    torch::nn::ConvTranspose2d  _convTranspose3;
+    torch::nn::BatchNorm2d      _bnDec3;
+    torch::nn::ConvTranspose2d  _convTranspose2;
+    torch::nn::ConvTranspose2d  _convTranspose1;
 };
 TORCH_MODULE(AutoEncoder);
