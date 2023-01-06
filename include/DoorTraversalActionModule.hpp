614187b460d91deceae1efc692c0aecc5773c08a
Lehdari <miika.lehtimaki@gmail.com>
2023-01-06 22:31:44 +0200

    DoorTraversalActionModule: Add comments

--- include/DoorTraversalActionModule.hpp
+++ include/DoorTraversalActionModule.hpp
@@ -13,8 +13,13 @@
 #include "ActionManager.hpp"
 
 
+// ActionManager module for simple door traversal procedures (including escape
+// from the oblige start rooms)
+// It also provides a minimal ActionManager module implementation example with
+// only the absolutely necessary components
 class DoorTraversalActionModule {
 public:
+    // Public state struct which is fetchable via ActionManager::getModuleState
     struct State {
         int forwardHoldTimer    {0};
         int wallHitTimer        {0};
@@ -22,12 +27,19 @@ public:
 
     DoorTraversalActionModule();
 
+    // Reset is called in game restarts. Should reset the state
     void reset();
 
+    // Iteration. Called by ActionManager during its call
+    // callParams:      Call parameters provided into the ActionManager call. Include
+    //                  information about the game state
+    // updateParams:    Parameters used for producing the next action. Primary product
+    //                  of the module calling process
+    // actionManager:   Reference to the ActionManager instance performing the call
     void operator()(
         const ActionManager::CallParams& callParams,
         ActionManager::UpdateParams& updateParams,
         ActionManager& actionManager);
 
-    State   state;
+    State   state; // public state member
 };
