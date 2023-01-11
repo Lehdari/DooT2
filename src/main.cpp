a5cb79c01a37bfd3379a02b9fffdbdcd4c40c79d
Lehdari <miika.lehtimaki@gmail.com>
2023-01-11 22:39:16 +0200

    Disable HUD and weapon rendering

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,11 @@
 
 int main()
 {
-    gvizdoom::GameConfig config;
+    gvizdoom::GameConfig config{0, nullptr, false, true,
+        640, 480, true,
+        gvizdoom::GameConfig::HUD_DISABLED, 2, false,
+        3, 1, 1
+    };
     config.pwadFileNames = {{assetsDir/"wads"/"oblige01.wad"}};
 
     auto& doomGame = gvizdoom::DoomGame::instance();
