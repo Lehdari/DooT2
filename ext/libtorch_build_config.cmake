ba0281aff69053a37093e517ca79140791974f24
Lehdari <miika.lehtimaki@gmail.com>
2023-02-11 23:52:12 +0200

    CMake: Integrate pytorch building

---
+++ ext/libtorch_build_config.cmake
@@ -0,0 +1,6 @@
+# libtorch build config is done via environment variables
+set(ENV{BUILD_PYTHON} 0)
+set(ENV{USE_NUMPY} 0)
+set(ENV{BUILD_CAFFE2_OPS} 0)
+set(ENV{BUILD_TEST} 0)
+set(ENV{USE_CUDNN} 1)
