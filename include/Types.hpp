b0491e3d7bc0b38cfe76bac72585cff6dadec1fc
Lehdari <miika.lehtimaki@gmail.com>
2023-02-16 22:52:13 +0200

    Add Types.hpp
    For typedefs

---
+++ include/Types.hpp
@@ -0,0 +1,18 @@
+//
+// Project: DooT2
+// File: Types.hpp
+//
+// Copyright (c) 2023 Miika 'Lehdari' Lehtimäki
+// You may use, distribute and modify this code under the terms
+// of the licence specified in file LICENSE which is distributed
+// with this source code package.
+//
+
+#pragma once
+
+#include <vector>
+
+#include <torch/torch.h>
+
+
+using TensorVector = std::vector<torch::Tensor>;
