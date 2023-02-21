f8e0e51c62b656ab409b9c5d3145b35db2a6d78f
Eljas Hyyrynen <eljas.hyyrynen@gmail.com>
2023-02-21 18:44:05 +0200

    Split app into Trainer and renderer App

--- include/App.hpp
+++ include/App.hpp
@@ -10,53 +10,20 @@
 
 #pragma once
 
-
-#include "ActionManager.hpp"
-#include "HeatmapActionModule.hpp"
-#include "DoorTraversalActionModule.hpp"
-#include "SequenceStorage.hpp"
-
 #include <SDL.h>
-#include <opencv2/core/mat.hpp>
-#include "gvizdoom/Action.hpp"
-
-#include <random>
-
-
-class Model;
-
 
 class App {
 public:
-    App(Model* model);
+    App();
     // TODO RO5
     ~App();
 
     void loop();
 
 private:
-    using Rnd = std::default_random_engine;
-
-    Rnd                         _rnd;
-
     SDL_Window*                 _window;
     SDL_Renderer*               _renderer;
     SDL_Texture*                _texture;
 
     bool                        _quit;
-    ActionManager               _actionManager;
-    HeatmapActionModule         _heatmapActionModule;
-    DoorTraversalActionModule   _doorTraversalActionModule;
-    SequenceStorage             _sequenceStorage;
-    cv::Mat                     _positionPlot;
-    Vec2f                       _initPlayerPos;
-
-    size_t                      _frameId;
-    size_t                      _batchEntryId;
-    bool                        _newPatchReady;
-
-    Model*                      _model;
-
-
-    void nextMap(); // proceed to next map
 };
