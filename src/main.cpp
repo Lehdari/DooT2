d1570515ac1b241bb9a7e68b1397aa9a9319a9e1
Eljas Hyyrynen <eljas.hyyrynen@gmail.com>
2023-03-06 20:09:18 +0200

    main: add CLI11 as submodule

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include "App.hpp"
+#include "CLI/CLI.hpp"
 #include "Trainer.hpp"
 #include "Utils.hpp"
 #include "AutoEncoderModel.hpp"
@@ -9,6 +10,12 @@
 
 int main()
 {
+    CLI::App cliApp{"DooT2 Machine Learning Research Platform"};
+    uint32_t cliBatchSize; 
+    cliApp.add_option("--batchsize,-b", cliBatchSize, "Batch size for the autoencoder model");
+
+    CLI11_PARSE(cliApp);
+
     gvizdoom::GameConfig config{0, nullptr, false, true,
         640, 480, true,
         gvizdoom::GameConfig::HUD_DISABLED, 2, false,
