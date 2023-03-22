4fed824a2bf31534386d0aa71f69fa39988ccb7c
Lehdari <miika.lehtimaki@gmail.com>
2023-03-23 01:18:17 +0200

    ml: Add EncoderModel

---
+++ include/ml/models/EncoderModel.hpp
@@ -0,0 +1,35 @@
+//
+// Project: DooT2
+// File: EncoderModel.hpp
+//
+// Copyright (c) 2023 Miika 'Lehdari' Lehtimäki
+// You may use, distribute and modify this code under the terms
+// of the licence specified in file LICENSE which is distributed
+// with this source code package.
+//
+
+#pragma once
+
+#include "ml/Model.hpp"
+#include "ml/modules/FrameEncoder.hpp"
+
+
+namespace ml {
+
+    class EncoderModel final : public Model {
+    public:
+        EncoderModel();
+        EncoderModel(const EncoderModel&) = delete;
+        EncoderModel(EncoderModel&&) = delete;
+        EncoderModel& operator=(const EncoderModel&) = delete;
+        EncoderModel& operator=(EncoderModel&&) = delete;
+
+        void infer(const TensorVector& input, TensorVector& output) override;
+
+    private:
+        FrameEncoder    _encoder;
+
+        void trainImpl(SequenceStorage& storage) override;
+    };
+
+} // namespace ml
---
+++ src/ml/models/EncoderModel.cpp
@@ -0,0 +1,29 @@
+//
+// Project: DooT2
+// File: EncoderModel.cpp
+//
+// Copyright (c) 2023 Miika 'Lehdari' Lehtimäki
+// You may use, distribute and modify this code under the terms
+// of the licence specified in file LICENSE which is distributed
+// with this source code package.
+//
+
+#include "ml/models/EncoderModel.hpp"
+
+
+using namespace ml;
+
+
+EncoderModel::EncoderModel()
+{
+}
+
+void EncoderModel::infer(const TensorVector& input, TensorVector& output)
+{
+    output.resize(1);
+    output[0] = _encoder->forward(input[0]);
+}
+
+void EncoderModel::trainImpl(SequenceStorage& storage)
+{
+}
