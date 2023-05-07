1d365cfe169166123c23e2e43f56d971449fca80
Lehdari <miika.lehtimaki@gmail.com>
2023-05-07 21:33:10 +0300

    ExperimentUtils: Add model parameter name macros

--- src/util/ExperimentUtils.cpp
+++ src/util/ExperimentUtils.cpp
@@ -32,6 +32,30 @@ std::string formatExperimentName(const std::string& name, const nlohmann::json& 
     // Replace {version} with git version hash
     experimentName = std::regex_replace(experimentName, std::regex("\\{version}"), GIT_VERSION);
 
+    // Replace all the parameter macros ({p:<parameter name>}) with the respective parameter values
+    std::regex paramRegex("\\{p:(.*?)}");
+    std::stringstream newExperimentNameSs;
+    std::string::const_iterator it = experimentName.cbegin(), end = experimentName.cend();
+    for (std::smatch match; std::regex_search(it, end, match, paramRegex); it = match[0].second)
+    {
+        newExperimentNameSs << match.prefix();
+        std::string param = match[1].str();
+        if (!modelConfig.contains(param)) {
+            printf("WARNING: Model config does not contain parameter \"%s\", ignoring the macro\n",
+                param.c_str()); // TODO logging
+            newExperimentNameSs << match.str(); // replace here
+        }
+        else {
+            if (modelConfig[param].type() == nlohmann::json::value_t::string)
+                newExperimentNameSs << modelConfig[param].get<std::string>();
+            else
+                newExperimentNameSs << modelConfig[param];
+        }
+    }
+    std::string newExperimentName = newExperimentNameSs.str();
+    newExperimentName.append(it, end);
+    experimentName = newExperimentName;
+
     return experimentName;
 }
 
