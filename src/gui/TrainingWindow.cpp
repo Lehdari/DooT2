e79b2cc8604a0a263ef2b765e68a661b6523c97a
Lehdari <miika.lehtimaki@gmail.com>
2023-05-08 01:21:16 +0300

    TrainingWindow: Add GUI for grid search

--- src/gui/TrainingWindow.cpp
+++ src/gui/TrainingWindow.cpp
@@ -34,6 +34,9 @@ void gui::TrainingWindow::update()
 void gui::TrainingWindow::render(ml::Trainer* trainer)
 {
     if (!_open) return;
+    int id = 0;
+    auto* experimentConfig = trainer->getExperimentConfig();
+
     if (ImGui::Begin(("Training " + std::to_string(_id)).c_str(), &_open)) {
         float fontSize = ImGui::GetFontSize();
         ImVec2 windowSize = ImGui::GetWindowSize();
@@ -41,6 +44,7 @@ void gui::TrainingWindow::render(ml::Trainer* trainer)
         // Flag for disabling all settings when training's in progress
         bool trainingInProgress = _guiState->trainingStatus != State::TrainingStatus::STOPPED;
 
+        // Experiment configuration
         if (ImGui::CollapsingHeader("Experiment configuration")) {
             ImGui::BeginDisabled(trainingInProgress);
 
@@ -105,11 +109,10 @@ void gui::TrainingWindow::render(ml::Trainer* trainer)
             ImGui::EndDisabled();
         }
 
-        // Model config table
+        // Model configuration
         if (ImGui::CollapsingHeader("Model configuration")) {
             ImGui::BeginDisabled(trainingInProgress);
 
-            auto* experimentConfig = trainer->getExperimentConfig();
             assert(experimentConfig != nullptr);
             if (experimentConfig->contains("model_config")) {
                 auto& modelConfig = (*experimentConfig)["model_config"];
@@ -117,7 +120,6 @@ void gui::TrainingWindow::render(ml::Trainer* trainer)
                     ImGuiTableFlags_Borders |
                     ImGuiTableFlags_SizingFixedFit |
                     ImGuiTableFlags_SizingFixedSame)) {
-                    int id = 0;
                     // List all config entries
                     ImGui::PushStyleVar(ImGuiStyleVar_ItemInnerSpacing, {0,0});
                     for (auto& [paramName, paramValue]: modelConfig.items()) {
@@ -163,6 +165,142 @@ void gui::TrainingWindow::render(ml::Trainer* trainer)
                 }
             }
 
+            ImGui::EndDisabled();
+        }
+
+        // Grid search setup
+        if (ImGui::CollapsingHeader("Grid search")) {
+            ImGui::BeginDisabled(trainingInProgress);
+            ImGui::Checkbox("Perform a grid search", &_guiState->gridSearch);
+
+            ImGui::BeginDisabled(!_guiState->gridSearch);
+
+            auto& gridSearchConfig = _guiState->gridSearchModelConfigParams;
+            int maxNParamValues = 0;
+            for (auto& [paramName, paramValues] : gridSearchConfig.items()) {
+                if (paramValues.size() > maxNParamValues) {
+                    maxNParamValues = paramValues.size();
+                }
+            }
+
+            if (experimentConfig->contains("model_config")) {
+                auto& modelConfig = (*experimentConfig)["model_config"];
+                if (ImGui::BeginTable("Grid search configuration", maxNParamValues+2,
+                    ImGuiTableFlags_Borders |
+                    ImGuiTableFlags_ScrollX |
+                    ImGuiTableFlags_Resizable)) {
+                    // List all config entries
+                    ImGui::PushStyleVar(ImGuiStyleVar_ItemInnerSpacing, {0,0});
+                    for (auto& [paramName, paramValues]: gridSearchConfig.items()) {
+                        ImGui::TableNextRow();
+                        ImGui::TableSetColumnIndex(0);
+
+                        // First column: the parameter selection
+                        ImGui::SetNextItemWidth(ImGui::GetColumnWidth()-fontSize*1.8f);
+                        ImGui::PushID(id++);
+                        bool paramChanged = false;
+                        if (ImGui::BeginCombo("##gridSearchParamName", paramName.c_str())) {
+                            for (auto& [pName, pValue] : modelConfig.items()) {
+                                bool isSelected = (pName == paramName);
+                                if (!gridSearchConfig.contains(pName) &&
+                                    ImGui::Selectable(pName.c_str(), isSelected)) {
+                                    gridSearchConfig.erase(paramName);
+                                    gridSearchConfig[pName] = { pValue };
+                                    paramChanged = true;
+                                }
+                            }
+                            ImGui::EndCombo();
+                        }
+                        // Parameter removal button
+                        ImGui::SameLine();
+                        if (ImGui::Button("X##removeGridSearchParam")) {
+                            gridSearchConfig.erase(paramName);
+                            paramChanged = true;
+                        }
+                        // Tooltip for the removal button
+                        if (ImGui::IsItemHovered(ImGuiHoveredFlags_AllowWhenDisabled))
+                            ImGui::SetTooltip("Remove the parameter");
+                        ImGui::PopID();
+                        if (paramChanged)
+                            break;
+
+                        // Find parameter type
+                        auto paramType = nlohmann::json::value_t::null; // null means parameter not found in model config
+                        if (modelConfig.contains(paramName))
+                            paramType = modelConfig[paramName].type();
+
+                        int columnId = 0;
+                        for (auto& paramValue : paramValues) {
+                            ImGui::TableSetColumnIndex(++columnId);
+                            float columnWidth = ImGui::GetColumnWidth();
+                            ImGui::PushID(id++);
+                            switch (paramType) {
+                                case nlohmann::json::value_t::boolean: {
+                                    bool v = paramValue.get<bool>();
+                                    ImGui::Checkbox("##value", &v);
+                                    paramValue = v;
+                                }   break;
+                                case nlohmann::json::value_t::number_integer:
+                                case nlohmann::json::value_t::number_unsigned: {
+                                    int v = paramValue.get<int>();
+                                    ImGui::SetNextItemWidth(columnWidth);
+                                    ImGui::InputInt("##value", &v);
+                                    paramValue = v;
+                                }   break;
+                                case nlohmann::json::value_t::number_float: {
+                                    double v = paramValue.get<double>();
+                                    ImGui::SetNextItemWidth(columnWidth);
+                                    ImGui::InputDouble("##value", &v, 0.0, 0.0, "%.5g");
+                                    paramValue = v;
+                                }   break;
+                                case nlohmann::json::value_t::string: {
+                                    std::string v = paramValue.get<std::string>();
+                                    ImGui::SetNextItemWidth(columnWidth);
+                                    if (ImGui::InputText("##value", &v))
+                                        paramValue = v;
+                                }   break;
+                                case nlohmann::json::value_t::null: {
+                                    ImGui::Text("No such model parameter");
+                                }   break;
+                                default: {
+                                    ImGui::Text("Unsupported type");
+                                }   break;
+                            }
+                            ImGui::PopID();
+                        }
+
+                        // Last column for adding values
+                        ImGui::TableSetColumnIndex(++columnId);
+                        ImGui::PushID(id++);
+                        if (ImGui::Button("Add")) {
+                            gridSearchConfig[paramName].push_back(paramValues.back());
+                        }
+                        ImGui::PopID();
+                    }
+
+                    // Last row for adding parameters
+                    ImGui::TableNextRow();
+                    ImGui::TableSetColumnIndex(0);
+                    {
+                        ImGui::PushID(id++);
+                        if (ImGui::BeginCombo("##gridSearchParamName", "Add parameter")) {
+                            for (auto& [pName, pValue] : modelConfig.items()) {
+                                if (!gridSearchConfig.contains(pName) &&
+                                    ImGui::Selectable(pName.c_str())) {
+                                    gridSearchConfig[pName] = { pValue };
+                                }
+                            }
+                            ImGui::EndCombo();
+                        }
+                        ImGui::PopID();
+                    }
+
+                    ImGui::PopStyleVar();
+                    ImGui::EndTable();
+                }
+            }
+
+            ImGui::EndDisabled();
             ImGui::EndDisabled();
         }
 
